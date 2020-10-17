6232f1bc0a3aac8b2cfb640389fb0bab6a3be23f
Chris Southwick <csouthwick@live.com>
2020-10-17 17:59:23 -0400

    added html routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 
 // parse incomping string or array data
@@ -7,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
-
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
