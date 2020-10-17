97f295606e47850fea0187320b2f3d5a93d10510
Chris Southwick <csouthwick@live.com>
2020-10-17 17:50:24 -0400

    require express and middleware to parse incoming JSON

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// parse incomping string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
