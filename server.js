95c798f04ab2faaca2ecf4dcd2fac3e0bf4981fd
Chris Southwick <csouthwick@live.com>
2020-10-17 18:32:40 -0400

    added route to read notes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const app = express();
 
 // parse incomping string or array data
@@ -8,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
